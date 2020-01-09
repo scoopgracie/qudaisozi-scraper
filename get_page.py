006e2ba790f45a0569f39398d67190bd8a2b4629
scoopgracie <scoopgracie@gmail.com>
2020-01-09 13:13:16 -0800

    Only cache pages that need it Only pages ending in /1 need caching, and cached pages can be deleted after one retreival

--- get_page.py
+++ get_page.py
@@ -8,12 +8,15 @@ class NotFoundError(Exception):
 cache = {}
 def get(url):
     if url in cache.keys():
-        return cache[url] #Get cached page if it exists
+        response = cache[url]
+        del cache[url] #Only use a cached page once
+        return response #Get cached page if it exists
     else:
         request = requests.get(url)
         if request.status_code == 200:
             soup = BeautifulSoup(request.text, features='lxml')
-            cache[url] = soup #Cache the page
+            if url.endswith('/1'):
+                cache[url] = soup #Cache the page
             return soup
         else:
             raise NotFoundError
